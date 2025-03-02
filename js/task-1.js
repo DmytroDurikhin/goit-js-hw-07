1ec52715e1f7ce382e3416255b0b09fa11cf065e
Dmytro Durikhin <durihindmitriy@gmail.com>
2025-03-02 16:41:06 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -8,7 +8,7 @@ console.log(`Number of categories: ${items.length}`);
 
 
 items.forEach((item) => {
-  const title = document.querySelector("h2").textContent;
+  const title = item.querySelector("h2").textContent;
   const categoriesCount = item.querySelectorAll(" ul li").length;
   console.log(`Category: ${title}`);
   console.log(`Elements: ${categoriesCount}`);
